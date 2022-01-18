3397e08251b9f7f944f267cff6833e77f8055fe5
ArrthurGeronimo <artha.desenvolvimento@gmail.com>
2022-01-18 16:50:42 -0400

    new feature

--- screens/index.js
+++ screens/index.js
@@ -1,11 +1,26 @@
 import { Center, Heading } from '@chakra-ui/react'
 
+import { Search_1 } from 'components/atoms/Search/Basic'
+
 export default function SetupPage() {
+
 	return (
+
 		<main>
+
 			<Center h="100vh">
+
 				<Heading>Setup Page</Heading>
+
+
+
+				<Search_1 />
+
 			</Center>
+
 		</main>
+
 	)
+
 }
+
