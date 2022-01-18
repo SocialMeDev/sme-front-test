2f6e982e7778a87c50891238ede19d05c1511752
ArrthurGeronimo <artha.desenvolvimento@gmail.com>
2022-01-18 16:53:34 -0400

    alterei

--- src/components/atoms/Search/Basic/_1/index.jsx
+++ src/components/atoms/Search/Basic/_1/index.jsx
@@ -1,37 +1,37 @@
-import { forwardRef, memo } from "react";
-import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
+import { forwardRef, memo } from 'react'
+import { Input, InputGroup, InputLeftElement } from '@chakra-ui/react'
 
-import { Search1 } from "components/atoms/Icons/Interface";
+import { Search1 } from 'components/atoms/Icons/Interface'
 
-import useDebounce from "hooks/useDebounce";
+import useDebounce from 'hooks/useDebounce'
 
 const SearchInput = forwardRef(({ onChange, onBlur, name, ...rest }, ref) => {
-  const handleOnChangeDebounce = useDebounce((event) => {
-    onChange(event);
-  });
+	const handleOnChangeDebounce = useDebounce((event) => {
+		onChange(event)
+	})
 
-  return (
-    <InputGroup
-      maxW={{ base: "100%", md: "250px" }}
-      mb={{ base: "4", md: "0" }}
-      {...rest}
-	  backgroundColor="red"
-    >
-      <InputLeftElement>
-        <Search1 transition="all 0.4s" boxSize={6} color="gray.300" />
-      </InputLeftElement>
-      <Input
-        color="red"
-        ref={ref}
-        onBlur={onBlur}
-        name={name}
-        onChange={handleOnChangeDebounce}
-        rounded="md"
-        autoComplete="off"
-        placeholder="Pesquisar"
-      />
-    </InputGroup>
-  );
-});
+	return (
+		<InputGroup
+			maxW={{ base: '100%', md: '250px' }}
+			mb={{ base: '4', md: '0' }}
+			{...rest}
+			backgroundColor="blue"
+		>
+			<InputLeftElement>
+				<Search1 transition="all 0.4s" boxSize={6} color="gray.300" />
+			</InputLeftElement>
+			<Input
+				color="red"
+				ref={ref}
+				onBlur={onBlur}
+				name={name}
+				onChange={handleOnChangeDebounce}
+				rounded="md"
+				autoComplete="off"
+				placeholder="Pesquisar"
+			/>
+		</InputGroup>
+	)
+})
 
-export default memo(SearchInput);
+export default memo(SearchInput)
